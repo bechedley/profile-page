8d0249058799935a75ede73f0f91b4884fd059f1
bechedley <109792989+bechedley@users.noreply.github.com>
2023-01-22 18:40:30 +1100

    Add resume download

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../styles/Page.css';
+import iconGif from '../../assets/images/web-dev-icons-gif.gif'
 
 export default function Resume() {
     return (
@@ -7,30 +8,37 @@ export default function Resume() {
             <h1>Resume</h1>
             <p>Click the link below to download a copy of my resume.
             </p>
-            <h3>Front-end Proficiencies</h3>
+            <a href="/files/web-dev-resume-rhedley.pdf" download>DOWNLOAD MY RESUME</a>
+            <div className='icon-top'>
+            <img src={iconGif} title="Icons of tech proficiencies" alt="icons of tech proficiencies" />
+            </div>
+            <div className='d-flex p3 page-box'>
+            <h3>Front-end Proficiencies:</h3>
             <ul>
                 <li>HTML</li>
                 <li>CSS</li>
                 <li>Responsive design</li>
+                <li>Bootstrap and CSS Frameworks</li>
                 <li>Javascript</li>
                 <li>Handlebars.js</li>
             </ul>
-            <h3>Back-end Proficiencies</h3>
+            <h3>Back-end Proficiencies:</h3>
             <ul>
                 <li>Git</li>
                 <li>Web APIs</li>
                 <li>Third-party APIs</li>
                 <li>Server-side APIs</li>
                 <li>Node.js</li>
-                <li>ES6</li> 
                 <li>Express.js</li>
-                <li>MySQL</li>
+                <li>SQL</li>
                 <li>Sequelize</li>
                 <li>NoSQL</li>
                 <li>MongoDB</li>
                 <li>Progressive Web Applications</li>
                 <li>React</li>
+                <li>MERN</li>
             </ul>
+            </div>
         </div>
     );
 }
